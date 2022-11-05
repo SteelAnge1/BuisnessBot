9edfe7db4f7a30194b6ebfae3540147744d906a8
SemyonC20 <semenmalahov906@gmail.com>
2022-11-05 18:26:27 +0700

    Add Base logic for tg bot

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-import config
-import logging
+from aiogram import Bot
+from aiogram.types import *
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+from config import TOKEN
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def echo(message: Message):
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
+
